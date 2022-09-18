a44ae0cbd7f2a79a8df1918e1b9564f0cba14242
abrahamzaragoza <abraham.zaragoza@enroutesystems.com>
2022-09-18 01:58:17 -0500

    Add notes to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,12 +9,15 @@
 #  category_id :bigint           not null
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  owner_id    :bigint           not null
+#  code        :string
 #
 class Task < ApplicationRecord
   belongs_to :category
   belongs_to :owner, class_name: 'User'
   has_many :participant_users, class_name: 'Participant'
   has_many :participants, through: :participant_users, source: :user
+  has_many :notes
 
   validates :participant_users, presence: true
   validates :name, presence: true, uniqueness: { case_sensitive: false }
@@ -22,6 +25,7 @@ class Task < ApplicationRecord
   validate :due_date_validity
 
   before_create :create_code
+  after_create :send_email
 
   accepts_nested_attributes_for :participant_users, allow_destroy: true
 
@@ -34,5 +38,11 @@ class Task < ApplicationRecord
 
   def create_code
     self.code = "#{owner.id}#{Time.now.to_i.to_s(36)}#{SecureRandom.hex(8)}"
+  end
+
+  def send_email
+    (participants + [owner]).each do |user|
+      ParticipantMailer.with(user: user, task: self).new_task_email.deliver!
+    end
   end
 end
